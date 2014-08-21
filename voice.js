c2699e822989d704156248d24c866717ea9512af
Brett Michaelis <brett.michaelis@gmail.com>
2014-08-21 12:58:15 -0600

    updated the array in getCookie(). It was not working properly and after the additon of the '_jar' array element it is working again

--- voice.js
+++ voice.js
@@ -3,7 +3,7 @@ var EE = require('events').EventEmitter,
     clientlogin = require('googleclientlogin'),
     request = require('request'),
     xml2js = require('xml2js');
-	
+
 clientlogin.GoogleClientLogin.prototype.loginWithCallback = function(callback){
 	var self = this;
 	callback = callback || noop;
@@ -34,16 +34,16 @@ exports.Client = function(options){
 		email: options.email,
 		password: options.password
 	};
-	
+
 	this._clientlogin = new clientlogin.GoogleClientLogin({
 		email: options.email,
 		password: options.password,
 		service:'voice'
 	});
-	
+
 
 	var tokens = {}; // protected variable
-	
+
 	if(options.tokens){ // populate tokens
 		for(var name in options.tokens){
 			if(!!~DEFAULTS.tokenTypes.indexOf(name)){
@@ -51,7 +51,7 @@ exports.Client = function(options){
 			}
 		}
 	}
-	
+
 	this.getTokens = function(name){
 		if(name){
 			return tokens[name];
@@ -63,7 +63,7 @@ exports.Client = function(options){
 		}
 		return toks;
 	};
-	
+
 	this._setToken = function(name, value){
 		if(!!~DEFAULTS.tokenTypes.indexOf(name)){
 			tokens[name] = value;
@@ -79,12 +79,12 @@ exports.Client.prototype = Object.create(EE.prototype);
 exports.Client.prototype.auth = function(callback){
 	var self = this;
 	callback = callback || noop;
-	
+
 	if(this._processing_auth){ // if a login is already in progress, wait for it to finish
 		self.once('_processed_auth', callback);
 		return;
 	}
-	
+
 	this._processing_auth = true;
 	this._clientlogin.loginWithCallback(function(err, authToken){
 		self._processing_auth = false;
@@ -115,14 +115,14 @@ exports.Client.prototype._checkTokenStatus = function(token, callback){
 exports.Client.prototype.rnr = function(callback, isRepeat){
 	var self = this;
 	callback = callback || noop;
-	
+
 	if(this._processing_rnr){ // if a getRNR is already in progress, wait for it to finish
 		self.once('_processed_rnr', callback);
 		return;
 	}
-	
+
 	this._processing_rnr = true;
-	
+
 	if(self._checkTokenStatus('auth', function(err){// check if we have an auth token OR if a login is in process
 		if(err){
 			return callback(err);
@@ -130,15 +130,15 @@ exports.Client.prototype.rnr = function(callback, isRepeat){
 		self._processing_rnr = false;
 		self.rnr(callback);
 	})){ return; }; // wait for auth token retrieval, as this will not work without it
-	
-	
+
+
 	request({
 		uri: 'https://www.google.com/voice/request/unread', // this url contains unread counts as well as the rnr token
 		followRedirect: false,
 		headers: {
 			Authorization: 'GoogleLogin auth=' + this.getTokens().auth // Google ClientLogin authentication
 		}
-	}, function(err, res, body){		
+	}, function(err, res, body){
 		self._processing_rnr = false;
 		body = self._toJSON(body);
 		if(err){
@@ -173,7 +173,7 @@ exports.Client.prototype.rnr = function(callback, isRepeat){
 
 
 var getCookie = function(jar, name){
-	var parts = ['store', 'idx', 'www.google.com', '/voice', name];
+	var parts = ['_jar', 'store', 'idx', 'www.google.com', '/voice', name];
 	var obj = jar;
 	while (obj && parts.length) {
 		obj = obj[parts.shift()];
@@ -184,15 +184,15 @@ var getCookie = function(jar, name){
 exports.Client.prototype.gvx = function(callback, isRepeat){
 	var self = this;
 	callback = callback || noop;
-	
-	
+
+
 	if(this._processing_gvx){ // if a getGVX is already in progress, wait for it to finish
 		self.once('_processed_gvx', callback);
 		return;
 	}
-	
+
 	this._processing_gvx = true;
-	
+
 	if(self._checkTokenStatus('auth', function(err){// check if we have an auth token OR if a login is in process
 		if(err){
 			return callback(err)
@@ -200,31 +200,31 @@ exports.Client.prototype.gvx = function(callback, isRepeat){
 		self._processing_gvx = false;
 		self.gvx(callback);
 	})){ return; } // wait for auth token retrieval, as getGVX will fail without it
-	
+
 	var jar = request.jar(); // the cookie jar
 	request({
 		uri: 'https://www.google.com/voice/m?initialauth&pli=1',
 		followRedirect: false,
 		jar: jar,
-		headers: { 
+		headers: {
 			'User-Agent': DEFAULTS.mobileHeader,
 			Authorization: 'GoogleLogin auth=' + this.getTokens().auth // Google ClientLogin authentication
 		}
 	},function(err, res, body){
-		self._processing_gvx = false;		
-		if(err){			
+		self._processing_gvx = false;
+		if(err){
 			var e = new GoogleVoiceError('REQUEST_ERROR', {error: err, caller: 'getGVX', res: res, body: body});
 			self.emit('_processed_gvx', e);
 			return callback(e, null, res, body);
 		}
-		
+
 		if(res.statusCode != 200 && res.statusCode != 302 && !isRepeat){ // Could be a login issue, try to login if this getGVX is NOT a retry
 			return self.auth(function(err){
 				if(err){
 					var e = new GoogleVoiceError('GET_GVX_TOKEN_ERROR', {res: res, body: body, afterLogin: true});
 					self.emit('_processed_gvx',e);
 					return callback(e);
-				}				
+				}
 				self.gvx(callback, true);
 			});
 		}
@@ -254,14 +254,14 @@ exports.Client.prototype._request = function(options, callback, neededToken){
 		}
 		self._request(options, callback, neededToken);
 	})){ return; } // wait for auth token retrieval
-	
+
 	if(neededToken && self._checkTokenStatus(neededToken, function(err){// check if we have the necessary token OR if one is being retrieved
 		if(err){
 			return callback(err);
 		}
 		self._request(options, callback, neededToken);
 	})){ return; } // wait for the token
-	
+
 	var requestOptions = {
 		uri: options.uri || DEFAULTS.baseUrl,
 		method: options.method || options.form ? 'POST' : 'GET',
@@ -270,7 +270,7 @@ exports.Client.prototype._request = function(options, callback, neededToken){
 			Authorization: 'GoogleLogin auth=' + self.getTokens().auth
 		}
 	};
-	
+
 	if(neededToken && neededToken === 'gvx'){
 		requestOptions.uri += '/m/x';
 		requestOptions.method = 'POST';
@@ -292,7 +292,7 @@ exports.Client.prototype._request = function(options, callback, neededToken){
 			requestOptions.form = options.form;
 		}
 	}
-	
+
 	if(options.query){
 		requestOptions.uri += '?' + querystring.stringify(options.query);
 	}
@@ -300,7 +300,7 @@ exports.Client.prototype._request = function(options, callback, neededToken){
 	if(options.hasOwnProperty('encoding')){
 		requestOptions.encoding = options.encoding;
 	}
-	
+
 	request(requestOptions, function(err, res, body){
 		self._processResponse(options, callback, neededToken, err, res, body);
 	});
@@ -320,12 +320,12 @@ exports.Client.prototype._toJSON = function(body){
 
 exports.Client.prototype._processResponse = function(options, callback, neededToken, err, res, body){
 	var self = this;
-	
+
 	if(err){
 		var e = new GoogleVoiceError('REQUEST_ERROR', {error: err, caller: '_processResponse', res: res, body: body});
 		callback(e)
 		return;
-	}else if(res.statusCode == 400 || res.statusCode === 401 || res.statusCode === 403){ // Possible authentication issue		
+	}else if(res.statusCode == 400 || res.statusCode === 401 || res.statusCode === 403){ // Possible authentication issue
 		if(!options.renewedLogin){
 			options.renewedLogin = true;
 			self.once('_processed_auth', function(err){
@@ -334,10 +334,10 @@ exports.Client.prototype._processResponse = function(options, callback, neededTo
 				}
 				self._request(options, callback, neededToken); // repeat the request after login is finished and successful
 			});
-			if(!self._processing_auth){				
+			if(!self._processing_auth){
 				self.auth();
 			}
-		}else{ // may need new rnr or gvx token	
+		}else{ // may need new rnr or gvx token
 			tryRenewToken('AUTHENTICATION_ERROR')
 		}
 	}else if(res.statusCode != 200){ //could be an rnr or gvx issue, so we'll try to renew those first
@@ -345,7 +345,7 @@ exports.Client.prototype._processResponse = function(options, callback, neededTo
 			return callback(new GoogleVoiceError('HTTP_ERROR', {res: res, body: body}));
 		}
         tryRenewToken('HTTP_ERROR');
-	}else{ // good response	
+	}else{ // good response
 		if(res && res.headers && res.headers['content-type'] && !!~res.headers['content-type'].indexOf('xml')){ // if xml
 			return new xml2js.Parser().parseString(body, function(err, xml){
 				if(err){
@@ -365,7 +365,7 @@ exports.Client.prototype._processResponse = function(options, callback, neededTo
 		}else{
 			body = self._toJSON(body);
 		}
-		
+
 		if(body && body.hasOwnProperty('rnr_xsrf_token') && self.getTokens().rnr != body.rnr_xsrf_token ){ // new rnr has been issued
 			self._setToken('rnr', body.rnr_xsrf_token)
 			self.emit('rnr', body.rnr_xsrf_token);
@@ -382,9 +382,9 @@ exports.Client.prototype._processResponse = function(options, callback, neededTo
 				}
 			});
 			callback(null, res, body);
-		}	
+		}
 	}
-	
+
 	function tryRenewToken(error){
 		if(!neededToken || options.renewedToken){ // Didn't need a token or Already tried renewing the tokens,so some other issue is the problem.
 			var e = new GoogleVoiceError(error, {res: res, body: body});
@@ -392,7 +392,7 @@ exports.Client.prototype._processResponse = function(options, callback, neededTo
         }
 		// try renewing the appropriate token
 		options.renewedToken = true;
-		self[neededToken](function(err){			
+		self[neededToken](function(err){
 			if(err){
 				return callback(err);
 			}
@@ -402,22 +402,22 @@ exports.Client.prototype._processResponse = function(options, callback, neededTo
 };
 
 exports.Client.prototype._exec = function(methods, command, options, callback){
-	if(!command || !methods[command]){ 
-		return callback(new GoogleVoiceError('INVALID_METHOD', {method: command, expected: Object.keys(methods)})); 
+	if(!command || !methods[command]){
+		return callback(new GoogleVoiceError('INVALID_METHOD', {method: command, expected: Object.keys(methods)}));
 	}
-	
+
 	if(typeof options === 'function'){
 		callback = options;
 	}
 	callback = callback || noop;
 	options = options || {};
-	
+
 	var method = methods[command];
-	
+
 	if(typeof method === 'function'){ //hand it off to the custom handler
 		return method.call(this, options, callback);
 	}
-	
+
 	// set fixed parameters, check for missing parameters
 	for(var name in method.options){
 		if(method.options[name].fixed){
@@ -426,10 +426,10 @@ exports.Client.prototype._exec = function(methods, command, options, callback){
 			return callback(new GoogleVoiceError('MISSING_REQUIRED_PARAMETER', { parameter: name } )) ;
 		}
 	}
-	
-	
+
+
 	var parameters = {}; // the parameters that will be submitted
-	
+
 	for(var name in method.options){
 		if(!options.hasOwnProperty(name) || options[name] === null){
 			if(method.options[name].hasOwnProperty('default')){
@@ -438,7 +438,7 @@ exports.Client.prototype._exec = function(methods, command, options, callback){
 				}else{
 					var value = method.options[name].default;
 				}
-				
+
 				if(typeof value === 'undefined' || value === null){
     				continue;
 				}else{
@@ -456,12 +456,12 @@ exports.Client.prototype._exec = function(methods, command, options, callback){
 				return callback(new GoogleVoiceError('INVALID_PARAMETER_TYPE', {parameter: name, type: passedType, expected: requiredTypes, value: options[name]}));
 			}
 		}
-		
+
 		// check if passed options matches a list of possible matches
 		if(method.options[name].match && !~method.options[name].match.indexOf(options[name])){
 			return callback(new GoogleVoiceError('INVALID_PARAMETER_VALUE', {parameter:name, value: options[name], expected:method.options[name].match}));
 		}
-		
+
 		// finally, set the parameter name and value for parameters that will be sumitted
 		if(method.options[name].hasOwnProperty('submit') && method.options[name].submit === false){
 			continue; // this is a parameter that wil not be part of the form or the query
@@ -480,16 +480,16 @@ exports.Client.prototype._exec = function(methods, command, options, callback){
 			parameters[name] = options[name];
 		}
 	}
-	
+
 	for(var name in parameters){
 		if(typeof parameters[name] === 'boolean'){ // map Booleans to 1 and 0
 			parameters[name] = parameters[name] ? 1 : 0;
 		}
 	}
-	
+
 	var neededToken = method.token;
 	var requestOptions = {};
-	
+
 	if(method.path){
 		if(typeof method.path === 'function'){
 			requestOptions.path = method.path.call(this, options);
@@ -502,7 +502,7 @@ exports.Client.prototype._exec = function(methods, command, options, callback){
 	}
 	if(method.hasOwnProperty('encoding')){
 		requestOptions.encoding = method.encoding;
-	}	
+	}
 	if(Object.keys(parameters).length){
 		if(method.method === 'POST' || neededToken && neededToken === 'rnr'){
 			requestOptions.form = parameters;
@@ -522,7 +522,7 @@ function createMethod(name){
 }
 
 for(var name in methods){
-	exports.Client.prototype[name] = createMethod(name);	
+	exports.Client.prototype[name] = createMethod(name);
 }
 
 // convenience methods
@@ -556,12 +556,12 @@ exports.Client.prototype.getAudio = function(options, callback){
 
 exports.Client.prototype.getCounts = function(callback){
 	callback = callback || noop;
-	
+
 	this._request({
 		query: {
-			m:'list', 
-			l: 'inbox', 
-			o: 0, 
+			m:'list',
+			l: 'inbox',
+			o: 0,
 			v: 10
 		}
 	}, function(error, response, data){
@@ -571,7 +571,7 @@ exports.Client.prototype.getCounts = function(callback){
 		if(data && data.labels_response && data.labels_response.label){
 			callback(null, data.labels_response.label, response, data)
 		}else{
-			callback(new GoogleVoiceError('UNKNOWN_FORMAT'), null, response, data); 
+			callback(new GoogleVoiceError('UNKNOWN_FORMAT'), null, response, data);
 		}
 	}, 'gvx');
 };
